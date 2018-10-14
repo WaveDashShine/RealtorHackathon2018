d7a7e761bb48716f70cccea54f4ec0384ee41604
Erik <erikcui9020@gmail.com>
2018-10-14 13:23:36 -0400

    moved morty along with the logos

--- Morty/morty.js
+++ Morty/morty.js
@@ -17,7 +17,7 @@ window.onload = function() {
     var insertLocation = document.querySelector('.m_property_dtl_realtor_cell'); // TODO: this is either supposed to have "cell" or the class below it
     var buttons = document.createElement('div');
     var tdUrl = "https://tools.td.com/mortgage-affordability-calculator" + "?city=" + cityProvince;
-    buttons.innerHTML = '    <div class="m_property_dtl_realtor_info" id="morty_anchor">\n' +
+    buttons.innerHTML = '    <div class="m_property_dtl_realtor_info">\n' +
         '        <div class="m_property_dtl_realtor_info_lft">\n' +
         '        <a id="lnkRealtorDetails" href="Agent/2024786/Marcus-Noel-485-INDUSTRIAL-AVENUE-OTTAWA-ON-K1G0Z1"></a>\n' +
         '\n' +
@@ -41,7 +41,7 @@ window.onload = function() {
         '        <a id="lnkOfficeLogo" href=' + tdUrl +'><img src="https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/eaec155b966f93828bb87eccd6101299" id="imgOfficePhoto" class="m_realtor_dtl_office_img" alt="REALTOR®" title="RE/MAX CORE REALTY INC." style="\n' +
         '    width: 50px;\n' +
         '    "></a>\n' +
-        '    <a id="lnkOfficeLogo" href=' + tdUrl +'><img src="https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/eaec155b966f93828bb87eccd6101299" id="imgOfficePhoto" class="m_realtor_dtl_office_img" alt="REALTOR®" title="RE/MAX CORE REALTY INC." style="\n' +
+        '    <a  id="morty_anchor" href=' + tdUrl +'><img src="https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/eaec155b966f93828bb87eccd6101299" id="imgOfficePhoto" class="m_realtor_dtl_office_img" alt="REALTOR®" title="RE/MAX CORE REALTY INC." style="\n' +
         '    width: 50px;\n' +
         '    "></a></div></div>';
     // TODO: refactor
